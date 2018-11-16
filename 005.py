22fece64d4a52e676e90fb0d7f7393001b3bb1b1
Shadowmaple <1142319190@qq.com>
2018-11-16 16:45:11 +0800

    x

---
+++ 005.py
@@ -0,0 +1,14 @@
+class User():
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+class Adimin(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.privileges=['can add post', 'can delete post', 'can ban user']
+    def show_privileges(self):
+        for item in self.privileges:
+            print(item)
+
+adimin1 = Adimin('shadow', 'lawler')
+adimin1.show_privileges()
